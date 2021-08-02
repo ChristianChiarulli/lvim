874cb4a26328313f051b3e780d8641cd1e6003c6
Chris <chris@macbook.local>
2021-08-01 22:48:57 -0400

    fix extra comment

--- lua/user/blankline.lua
+++ lua/user/blankline.lua
@@ -37,6 +37,6 @@ vim.g.indent_blankline_context_patterns = {
   "import_statement",
   "operation_type",
 }
--- -- HACK: work-around for https://github.com/lukas-reineke/indent-blankline.nvim/issues/59
+-- HACK: work-around for https://github.com/lukas-reineke/indent-blankline.nvim/issues/59
 vim.wo.colorcolumn = "99999"
 
