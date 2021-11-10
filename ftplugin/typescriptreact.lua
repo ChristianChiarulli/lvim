d504415f3adc0c4c6b9b4e785a632c4a985cd8c9
christianchiarulli <chrisatmachine@gmail.com>
2021-11-09 20:53:15 -0500

    update typescriptreact formatter

--- ftplugin/typescriptreact.lua
+++ ftplugin/typescriptreact.lua
@@ -1,2 +1,20 @@
 local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup({{exe = "prettier", filetypes = {"typescriptreact"} }})
+formatters.setup {
+  {
+    exe = "prettier",
+    filetypes = { "typescriptreact" },
+    args = { "--no-semi", "--single-quote", "--jsx-bracket-same-line", "--jsx-single-quote" },
+  },
+}
+
+-- local linters = require "lvim.lsp.null-ls.linters"
+-- linters.setup({{exe = "eslint_d", filetypes = {"typescriptreact"} }})
+
+-- {
+--   "semi": true,
+--   "tabWidth": 2,
+--   "printWidth": 100,
+--   "singleQuote": true,
+--   "trailingComma": "none",
+--   "jsxBracketSameLine": true
+-- }
