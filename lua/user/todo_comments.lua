92514446c6b4aec15fc453c39f290432566ad5ea
ChristianChiarulli <chris.machine@pm.me>
2021-11-10 13:26:16 -0500

    update note color

--- lua/user/todo_comments.lua
+++ lua/user/todo_comments.lua
@@ -9,10 +9,10 @@ M.config = function()
 
   local error_red = '#F44747'
   local warning_orange = '#ff8800'
-  -- local info_yellow = '#FFCC66'
+  local info_yellow = '#FFCC66'
   local hint_blue = '#4FC1FF'
   local perf_purple = '#7C3AED'
-  local note_green = '#10B981'
+  -- local note_green = '#10B981'
 
   todo_comments.setup {
     signs = true, -- show icons in the signs column
@@ -29,7 +29,7 @@ M.config = function()
       HACK = { icon = " ", color = warning_orange },
       WARN = { icon = " ", color = warning_orange, alt = { "WARNING", "XXX" } },
       PERF = { icon = " ", color = perf_purple, alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
-      NOTE = { icon = " ", color = note_green, alt = { "INFO" } },
+      NOTE = { icon = " ", color = info_yellow, alt = { "INFO" } },
     },
     -- merge_keywords = true, -- when true, custom keywords will be merged with the defaults
     -- highlighting of the line containing the todo comment
