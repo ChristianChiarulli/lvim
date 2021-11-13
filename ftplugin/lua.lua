785f08a638f3d5fa20bf764f1b7ab7bb32175660
ChristianChiarulli <chris.machine@pm.me>
2021-11-13 18:34:08 -0500

    luacheck can be annoying

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -6,5 +6,5 @@
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup { { exe = "stylua", filetypes = { "lua" } } }
 
-local linters = require "lvim.lsp.null-ls.linters"
-linters.setup { { exe = "luacheck", filetypes = { "lua" } } }
+-- local linters = require "lvim.lsp.null-ls.linters"
+-- linters.setup { { exe = "luacheck", filetypes = { "lua" } } }
