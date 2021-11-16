f9907884a2729435d4edc0bb821eebe606dae3e5
ChristianChiarulli <chris.machine@pm.me>
2021-11-15 23:25:24 -0500

    solidity spaces -> 4

--- ftplugin/solidity.lua
+++ ftplugin/solidity.lua
@@ -7,3 +7,6 @@
 -- }
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup({{exe = "prettier", filetypes = {"solidity"} }})
+
+vim.cmd[[setlocal shiftwidth=4]]
+vim.cmd[[setlocal tabstop=4]]
