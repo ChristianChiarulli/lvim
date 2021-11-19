51bfe811cc8d83d87438f3c118ac3351a48ef0b1
ChristianChiarulli <chris.machine@pm.me>
2021-11-18 19:00:32 -0500

    update blankline

--- lua/user/blankline.lua
+++ lua/user/blankline.lua
@@ -55,8 +55,13 @@ M.config = function()
   -- vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
   -- vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
   -- vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+  -- vim.opt.list = true
+  -- vim.opt.listchars:append "space:⋅"
+  -- vim.opt.listchars:append "space:"
+  -- vim.opt.listchars:append "eol:↴"
 
   indent_blankline.setup {
+    -- show_end_of_line = true,
     -- space_char_blankline = " ",
     show_current_context = true,
     -- show_current_context_start = true,
