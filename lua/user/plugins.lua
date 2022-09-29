951d5f801f8b76c12af64a464209b38a95c7d57f
ChristianChiarulli <chris.machine@pm.me>
2022-09-29 00:00:01 -0400

    update plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,31 +1,33 @@
 -- Additional Plugins
 lvim.plugins = {
-  "mfussenegger/nvim-jdtls",
-  "rcarriga/nvim-dap-ui",
-  "karb94/neoscroll.nvim",
-  "j-hui/fidget.nvim",
-  "windwp/nvim-ts-autotag",
-  "kylechui/nvim-surround",
-  "christianchiarulli/harpoon",
-  "MattesGroeger/vim-bookmarks",
-  "NvChad/nvim-colorizer.lua",
-  "ghillb/cybu.nvim",
-  "moll/vim-bbye",
-  "folke/todo-comments.nvim",
-  "windwp/nvim-spectre",
-  "is0n/jaq-nvim",
-  {
-    "0x100101/lab.nvim",
-    run = "cd js && npm ci",
-  },
-  "f-person/git-blame.nvim",
-  "ruifm/gitlinker.nvim",
-  "mattn/vim-gist",
-  "mattn/webapi-vim",
-  "folke/zen-mode.nvim",
-  "lvimuser/lsp-inlayhints.nvim"
+	"mfussenegger/nvim-jdtls",
+	"rcarriga/nvim-dap-ui",
+	"karb94/neoscroll.nvim",
+	"j-hui/fidget.nvim",
+	"windwp/nvim-ts-autotag",
+	"kylechui/nvim-surround",
+	"christianchiarulli/harpoon",
+	"MattesGroeger/vim-bookmarks",
+	"NvChad/nvim-colorizer.lua",
+	"ghillb/cybu.nvim",
+	"moll/vim-bbye",
+	"folke/todo-comments.nvim",
+	"windwp/nvim-spectre",
+	"is0n/jaq-nvim",
+	{
+		"0x100101/lab.nvim",
+		run = "cd js && npm ci",
+	},
+	"f-person/git-blame.nvim",
+	"ruifm/gitlinker.nvim",
+	"mattn/vim-gist",
+	"mattn/webapi-vim",
+	"folke/zen-mode.nvim",
+	"lvimuser/lsp-inlayhints.nvim",
+	"lunarvim/darkplus.nvim",
+	"lunarvim/templeos.nvim",
 
-  -- https://github.com/jose-elias-alvarez/typescript.nvim
-  -- "rmagatti/auto-session",
-  -- "rmagatti/session-lens"
+	-- https://github.com/jose-elias-alvarez/typescript.nvim
+	-- "rmagatti/auto-session",
+	-- "rmagatti/session-lens"
 }
