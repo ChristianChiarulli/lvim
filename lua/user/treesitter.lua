5fb7e331138820481c7ee3a7c79758c01be4848b
Christian Chiarulli <chris.machine@pm.me>
2022-10-10 13:13:59 -0400

    treesitter text objects

---
+++ lua/user/treesitter.lua
@@ -0,0 +1,32 @@
+lvim.builtin.treesitter.textobjects = {
+  select = {
+    enable = true,
+    -- Automatically jump forward to textobj, similar to targets.vim
+    lookahead = true,
+    keymaps = {
+      -- You can use the capture groups defined in textobjects.scm
+      ["af"] = "@function.outer",
+      ["if"] = "@function.inner",
+      ["at"] = "@class.outer",
+      ["it"] = "@class.inner",
+      ["ac"] = "@call.outer",
+      ["ic"] = "@call.inner",
+      ["aa"] = "@parameter.outer",
+      ["ia"] = "@parameter.inner",
+      ["al"] = "@loop.outer",
+      ["il"] = "@loop.inner",
+      ["ai"] = "@conditional.outer",
+      ["ii"] = "@conditional.inner",
+      ["a/"] = "@comment.outer",
+      ["i/"] = "@comment.inner",
+      ["ab"] = "@block.outer",
+      ["ib"] = "@block.inner",
+      ["as"] = "@statement.outer",
+      ["is"] = "@scopename.inner",
+      ["aA"] = "@attribute.outer",
+      ["iA"] = "@attribute.inner",
+      ["aF"] = "@frame.outer",
+      ["iF"] = "@frame.inner",
+    },
+  },
+}
