e17d105a11944479f55d3a4f68e2bc6697935665
Christian Chiarulli <chris.machine@pm.me>
2022-10-10 17:01:46 -0400

    udpate

--- lua/user/neoscroll.lua
+++ lua/user/neoscroll.lua
@@ -5,7 +5,7 @@ end
 
 neoscroll.setup {
   -- All these keys will be mapped to their corresponding default scrolling animation
-  mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
+  mappings = { "C-j", "C-k", "<C-u>", "<C-d>", "<C-b>", "<C-f>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
   hide_cursor = true, -- Hide cursor while scrolling
   stop_eof = true, -- Stop at <EOF> when scrolling downwards
   use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
