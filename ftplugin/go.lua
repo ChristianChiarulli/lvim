e9a20fb9bdbecf59c6bec6e78a52dd0104e8f524
Christian Chiarulli <chris.machine@pm.me>
2022-10-15 20:29:14 -0400

    fix go

--- ftplugin/go.lua
+++ ftplugin/go.lua
@@ -16,14 +16,14 @@ local mappings = {
   C = {
     name = "Go",
     i = { "<cmd>GoInstallDeps<Cr>", "Install Go Dependencies" },
-    t = { "<cmd>GoMod tidy<cr>", "Tidy" },
+    f = { "<cmd>GoMod tidy<cr>", "Tidy" },
     a = { "<cmd>GoTestAdd<Cr>", "Add Test" },
     A = { "<cmd>GoTestsAll<Cr>", "Add All Tests" },
     e = { "<cmd>GoTestsExp<Cr>", "Add Exported Tests" },
     g = { "<cmd>GoGenerate<Cr>", "Go Generate" },
-    f = { "<cmd>GoGenerate %<Cr>", "Go Generate File" },
+    G = { "<cmd>GoGenerate %<Cr>", "Go Generate File" },
     c = { "<cmd>GoCmt<Cr>", "Generate Comment" },
-    T = { "<cmd>lua require('dap-go').debug_test()<cr>", "Debug Test" },
+    t = { "<cmd>lua require('dap-go').debug_test()<cr>", "Debug Test" },
   },
 }
 
