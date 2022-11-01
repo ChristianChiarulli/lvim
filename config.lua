3ecf4e45f2b1ebb6a6eafdd8e9f8507f78f5b5c5
Christian Chiarulli <chris.machine@pm.me>
2022-11-01 01:46:58 -0400

    formatting

--- config.lua
+++ config.lua
@@ -1,28 +1,27 @@
 lvim.colorscheme = "darkplus"
-reload("user.plugins")
-reload("user.options")
-reload("user.keymaps")
-reload("user.autocommands")
-reload("user.lsp")
-reload("user.neoscroll")
-reload("user.harpoon")
-reload("user.cybu")
-reload("user.surround")
-reload("user.bookmark")
-reload("user.todo-comments")
-reload("user.jaq")
-reload("user.lab")
-reload("user.git")
-reload("user.zen-mode")
-reload("user.inlay-hints")
-reload("user.whichkey")
-reload("user.telescope")
-reload("user.bqf")
-reload("user.dial")
-reload("user.numb")
-reload("user.treesitter")
-reload("user.neogit")
-reload("user.colorizer")
-reload("user.lualine")
-reload("user.icons")
-
+reload "user.plugins"
+reload "user.options"
+reload "user.keymaps"
+reload "user.autocommands"
+reload "user.lsp"
+reload "user.neoscroll"
+reload "user.harpoon"
+reload "user.cybu"
+reload "user.surround"
+reload "user.bookmark"
+reload "user.todo-comments"
+reload "user.jaq"
+reload "user.lab"
+reload "user.git"
+reload "user.zen-mode"
+reload "user.inlay-hints"
+reload "user.whichkey"
+reload "user.telescope"
+reload "user.bqf"
+reload "user.dial"
+reload "user.numb"
+reload "user.treesitter"
+reload "user.neogit"
+reload "user.colorizer"
+reload "user.lualine"
+reload "user.icons"
