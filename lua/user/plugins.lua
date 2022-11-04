cb30d08d3919b78cdea3f3d20d14c932b3e2f3dd
Christian Chiarulli <chris.machine@pm.me>
2022-11-04 10:00:57 -0400

    update

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,103 +1,102 @@
 -- Additional Plugins
 lvim.plugins = {
   "ellisonleao/gruvbox.nvim",
-	"nvim-treesitter/playground",
-	"nvim-treesitter/nvim-treesitter-textobjects",
+  "nvim-treesitter/playground",
+  "nvim-treesitter/nvim-treesitter-textobjects",
   "p00f/nvim-ts-rainbow",
-	"mfussenegger/nvim-jdtls",
-	"karb94/neoscroll.nvim",
-	-- "j-hui/fidget.nvim",
-	"windwp/nvim-ts-autotag",
-	"kylechui/nvim-surround",
-	"christianchiarulli/harpoon",
-	"MattesGroeger/vim-bookmarks",
-	"NvChad/nvim-colorizer.lua",
-	"ghillb/cybu.nvim",
-	"moll/vim-bbye",
-	"folke/todo-comments.nvim",
-	"windwp/nvim-spectre",
-	"f-person/git-blame.nvim",
-	"ruifm/gitlinker.nvim",
-	"mattn/vim-gist",
-	"mattn/webapi-vim",
-	"folke/zen-mode.nvim",
-	"lvimuser/lsp-inlayhints.nvim",
-	"lunarvim/darkplus.nvim",
-	"lunarvim/templeos.nvim",
-	"kevinhwang91/nvim-bqf",
-	"is0n/jaq-nvim",
-	"hrsh7th/cmp-emoji",
-	"ggandor/leap.nvim",
-	"nacro90/numb.nvim",
-	"TimUntersberger/neogit",
-	"sindrets/diffview.nvim",
-	"simrat39/rust-tools.nvim",
-	"olexsmir/gopher.nvim",
-	"leoluz/nvim-dap-go",
-	"mfussenegger/nvim-dap-python",
-	"jose-elias-alvarez/typescript.nvim",
-	"mxsdev/nvim-dap-vscode-js",
-	{
-		"saecki/crates.nvim",
-		tag = "v0.3.0",
-		requires = { "nvim-lua/plenary.nvim" },
-		config = function()
-			require("crates").setup({
-				null_ls = {
-					enabled = true,
-					name = "crates.nvim",
-				},
-			})
-		end,
-	},
-	{
-		"jinh0/eyeliner.nvim",
-		config = function()
-			require("eyeliner").setup({
-				highlight_on_key = true,
-			})
-		end,
-	},
-	{ "christianchiarulli/telescope-tabs", branch = "chris" },
-	"monaqa/dial.nvim",
-	{
-		"0x100101/lab.nvim",
-		run = "cd js && npm ci",
-	},
-	{ "tzachar/cmp-tabnine", run = "./install.sh" },
-	{
-		"zbirenbaum/copilot.lua",
-		-- event = { "VimEnter" },
-		config = function()
-			vim.defer_fn(function()
-				require("copilot").setup({
-					plugin_manager_path = os.getenv("LUNARVIM_RUNTIME_DIR") .. "/site/pack/packer",
-				})
-			end, 100)
-		end,
-	},
-	{
-		"zbirenbaum/copilot-cmp",
-		after = { "copilot.lua" },
-		config = function()
-			require("copilot_cmp").setup({
-				formatters = {
-					insert_text = require("copilot_cmp.format").remove_existing,
-				},
-			})
-		end,
-	},
-	-- Packer
-	"MunifTanjim/nui.nvim",
-	{
-		"folke/noice.nvim",
-		event = "VimEnter",
-		config = function()
-			require("noice").setup()
-		end,
-	},
+  "mfussenegger/nvim-jdtls",
+  "karb94/neoscroll.nvim",
+  -- "j-hui/fidget.nvim",
+  "windwp/nvim-ts-autotag",
+  "kylechui/nvim-surround",
+  "christianchiarulli/harpoon",
+  "MattesGroeger/vim-bookmarks",
+  "NvChad/nvim-colorizer.lua",
+  "ghillb/cybu.nvim",
+  "moll/vim-bbye",
+  "folke/todo-comments.nvim",
+  "windwp/nvim-spectre",
+  "f-person/git-blame.nvim",
+  "ruifm/gitlinker.nvim",
+  "mattn/vim-gist",
+  "mattn/webapi-vim",
+  "folke/zen-mode.nvim",
+  "lvimuser/lsp-inlayhints.nvim",
+  "lunarvim/darkplus.nvim",
+  "lunarvim/templeos.nvim",
+  "kevinhwang91/nvim-bqf",
+  "is0n/jaq-nvim",
+  "hrsh7th/cmp-emoji",
+  "ggandor/leap.nvim",
+  "nacro90/numb.nvim",
+  "TimUntersberger/neogit",
+  "sindrets/diffview.nvim",
+  "simrat39/rust-tools.nvim",
+  "olexsmir/gopher.nvim",
+  "leoluz/nvim-dap-go",
+  "mfussenegger/nvim-dap-python",
+  "jose-elias-alvarez/typescript.nvim",
+  "mxsdev/nvim-dap-vscode-js",
+  {
+    "saecki/crates.nvim",
+    tag = "v0.3.0",
+    requires = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("crates").setup {
+        null_ls = {
+          enabled = true,
+          name = "crates.nvim",
+        },
+      }
+    end,
+  },
+  {
+    "jinh0/eyeliner.nvim",
+    config = function()
+      require("eyeliner").setup {
+        highlight_on_key = true,
+      }
+    end,
+  },
+  { "christianchiarulli/telescope-tabs", branch = "chris" },
+  "monaqa/dial.nvim",
+  {
+    "0x100101/lab.nvim",
+    run = "cd js && npm ci",
+  },
+  { "tzachar/cmp-tabnine", run = "./install.sh" },
+  {
+    "zbirenbaum/copilot.lua",
+    -- event = { "VimEnter" },
+    config = function()
+      vim.defer_fn(function()
+        require("copilot").setup {
+          plugin_manager_path = os.getenv "LUNARVIM_RUNTIME_DIR" .. "/site/pack/packer",
+        }
+      end, 100)
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    after = { "copilot.lua" },
+    config = function()
+      require("copilot_cmp").setup {
+        formatters = {
+          insert_text = require("copilot_cmp.format").remove_existing,
+        },
+      }
+    end,
+  },
+  -- "MunifTanjim/nui.nvim",
+  -- {
+  --   "folke/noice.nvim",
+  --   event = "VimEnter",
+  --   config = function()
+  --     require("noice").setup()
+  --   end,
+  -- },
 
-	-- https://github.com/jose-elias-alvarez/typescript.nvim
-	-- "rmagatti/auto-session",
-	-- "rmagatti/session-lens"
+  -- https://github.com/jose-elias-alvarez/typescript.nvim
+  -- "rmagatti/auto-session",
+  -- "rmagatti/session-lens"
 }
