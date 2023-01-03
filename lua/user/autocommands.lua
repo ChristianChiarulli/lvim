82df266dea03eda879c1ea51a878f5493371ba63
Christian Chiarulli <chris.machine@pm.me>
2023-01-03 01:35:20 -0500

    update autocommands

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -7,6 +7,12 @@
 --   end,
 -- })
 
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+  callback = function()
+    vim.cmd "set formatoptions-=cro"
+  end,
+})
+
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = {
     "Jaq",
