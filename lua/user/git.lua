d763394fb360b4fe79d496b0457f5dcb49839820
Christian Chiarulli <chris.machine@pm.me>
2023-01-03 01:17:19 -0500

    update

--- lua/user/git.lua
+++ lua/user/git.lua
@@ -2,6 +2,8 @@ vim.g.gitblame_enabled = 0
 vim.g.gitblame_message_template = "<summary> • <date> • <author>"
 vim.g.gitblame_highlight_group = "LineNr"
 
+lvim.builtin.gitsigns.opts.attach_to_untracked = false
+
 vim.g.gist_open_browser_after_post = 1
 
 local status_ok, gitlinker = pcall(require, "gitlinker")
