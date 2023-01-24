93652b842a6984f58470c9a74a4c7071c9803244
Christian Chiarulli <chris.machine@pm.me>
2023-01-24 01:55:12 -0500

    some updates

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -5,6 +5,7 @@ lvim.lsp.diagnostics.float.focusable = true
 lvim.reload_config_on_save = false
 lvim.builtin.illuminate.active = false
 lvim.builtin.bufferline.active = false
+vim.g.indent_blankline_char = "▏"
 lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
@@ -37,9 +38,9 @@ lvim.builtin.cmp.formatting.source_names = {
   copilot = "",
   treesitter = "",
 }
-lvim.builtin.cmp.formatting = {
-    format = require("tailwindcss-colorizer-cmp").formatter
-}
+-- lvim.builtin.cmp.formatting = {
+--     format = require("tailwindcss-colorizer-cmp").formatter
+-- }
 -- lvim.builtin.autopairs.active = false
 -- vim.g.netrw_browse_split = 0
 -- vim.g.netrw_banner = 0
--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -1,48 +1,51 @@
 if lvim.colorscheme == "darkplus" then
-	lvim.builtin.treesitter.rainbow = {
-		enable = true,
-		extended_mode = false,
-		colors = {
-			"DodgerBlue",
-			"Orchid",
-			"Gold",
-		},
-		disable = { "html" },
-	}
+  lvim.builtin.treesitter.rainbow = {
+    enable = true,
+    extended_mode = false,
+    colors = {
+      "DodgerBlue",
+      "Orchid",
+      "Gold",
+    },
+    disable = { "html" },
+  }
 end
 
-lvim.builtin.treesitter.ensure_installed = "all"
+lvim.builtin.treesitter.ensure_installed = { "javascript" }
 lvim.builtin.treesitter.autotag.enable = true
+lvim.builtin.treesitter.auto_install = false
+
+lvim.builtin.treesitter.ignore_install = { "markdown" }
 
 lvim.builtin.treesitter.textobjects = {
-	select = {
-		enable = true,
-		-- Automatically jump forward to textobj, similar to targets.vim
-		lookahead = true,
-		keymaps = {
-			-- You can use the capture groups defined in textobjects.scm
-			["af"] = "@function.outer",
-			["if"] = "@function.inner",
-			["at"] = "@class.outer",
-			["it"] = "@class.inner",
-			["ac"] = "@call.outer",
-			["ic"] = "@call.inner",
-			["aa"] = "@parameter.outer",
-			["ia"] = "@parameter.inner",
-			["al"] = "@loop.outer",
-			["il"] = "@loop.inner",
-			["ai"] = "@conditional.outer",
-			["ii"] = "@conditional.inner",
-			["a/"] = "@comment.outer",
-			["i/"] = "@comment.inner",
-			["ab"] = "@block.outer",
-			["ib"] = "@block.inner",
-			["as"] = "@statement.outer",
-			["is"] = "@scopename.inner",
-			["aA"] = "@attribute.outer",
-			["iA"] = "@attribute.inner",
-			["aF"] = "@frame.outer",
-			["iF"] = "@frame.inner",
-		},
-	},
+  select = {
+    enable = true,
+    -- Automatically jump forward to textobj, similar to targets.vim
+    lookahead = true,
+    keymaps = {
+      -- You can use the capture groups defined in textobjects.scm
+      ["af"] = "@function.outer",
+      ["if"] = "@function.inner",
+      ["at"] = "@class.outer",
+      ["it"] = "@class.inner",
+      ["ac"] = "@call.outer",
+      ["ic"] = "@call.inner",
+      ["aa"] = "@parameter.outer",
+      ["ia"] = "@parameter.inner",
+      ["al"] = "@loop.outer",
+      ["il"] = "@loop.inner",
+      ["ai"] = "@conditional.outer",
+      ["ii"] = "@conditional.inner",
+      ["a/"] = "@comment.outer",
+      ["i/"] = "@comment.inner",
+      ["ab"] = "@block.outer",
+      ["ib"] = "@block.inner",
+      ["as"] = "@statement.outer",
+      ["is"] = "@scopename.inner",
+      ["aA"] = "@attribute.outer",
+      ["iA"] = "@attribute.inner",
+      ["aF"] = "@frame.outer",
+      ["iF"] = "@frame.inner",
+    },
+  },
 }
