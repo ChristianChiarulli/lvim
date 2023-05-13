d6e393ab1b88f45fb8d074ec48057d3b5d672f45
Christian Chiarulli <chris.machine@pm.me>
2023-05-12 23:23:19 -0400

    update

--- lua/user/todo-comments.lua
+++ lua/user/todo-comments.lua
@@ -3,4 +3,9 @@ if not status_ok then
   return
 end
 
-todo_comments.setup {}
+todo_comments.setup {
+  signs = false, -- show icons in the signs column
+  highlight = {
+    after = "empty", -- "fg" or "bg" or empty
+  },
+}
